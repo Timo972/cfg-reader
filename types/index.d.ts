ac46b0d6092d2f990a07ac544a766526339cd454
Timo972 <beckmann.timo@icloud.com>
2021-02-22 16:00:37 +0100

    comment module declaration out

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,26 +1,27 @@
 declare type ConfigValue = string | boolean | number | any | any[]
-declare module "cfg-reader" {
 
-    export const enum ValueType {
-        Boolean,
-        Number,
-        String,
-        List,
-        Dict
-    }
-
-    export class Config {
-        constructor(fileName: string);
-        public Get(key: string): ConfigValue;
-        /**
-         * @param key {string}
-         * @param value {any}
-         */
-        public Set(key: string, value: ConfigValue): void;
-        public Save(): boolean;
-        public GetOfType(key: string, type: ValueType | number): ConfigValue;
-    }
-}
+//declare module "cfg-reader" {
+//
+//    export const enum ValueType {
+//        Boolean,
+//        Number,
+//        String,
+//        List,
+//        Dict
+//    }
+//
+//    export class Config {
+//        constructor(fileName: string);
+//        public Get(key: string): ConfigValue;
+//        /**
+//         * @param key {string}
+//         * @param value {any}
+//         */
+//        public Set(key: string, value: ConfigValue): void;
+//        public Save(): boolean;
+//        public GetOfType(key: string, type: ValueType | number): ConfigValue;
+//    }
+//}
 
 export const enum ValueType {
     Boolean,
