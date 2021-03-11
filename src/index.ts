a290c6eb573c57950559278fb9f88076c20cbf1d
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-03-11 23:18:53 +0100

    remove console logs and add eslint/prettier to project

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,15 @@ export enum ValueType {
     Number,
     String,
     List,
-    Dict
+    Dict,
 }
 
 export class Config {
     private config: ConfigObject = null;
     private lineCache: string[] = [];
     constructor(public readonly path: string | ConfigObject) {
-        if (typeof (this.path) === 'string')
-            this.parse();
-        else
-            this.config = this.path;
+        if (typeof this.path === 'string') this.parse();
+        else this.config = this.path;
     }
     protected processDictOrList(line: string, seperator: string = null): any | null {
         if (line.includes(']')) {
@@ -27,9 +25,16 @@ export class Config {
             // end of list
             // process list through lineCache
             const key = this.lineCache[1];
-            const values = seperator == null ? this.lineCache.slice(2, this.lineCache.length).filter(x => x !== '[') : this.lineCache.slice(2, this.lineCache.length).filter(x => x !== '[').join(seperator).trim().replace(/\s/g, '').split(seperator);
-
-            console.log(values);
+            const values =
+                seperator == null
+                    ? this.lineCache.slice(2, this.lineCache.length).filter((x) => x !== '[')
+                    : this.lineCache
+                          .slice(2, this.lineCache.length)
+                          .filter((x) => x !== '[')
+                          .join(seperator)
+                          .trim()
+                          .replace(/\s/g, '')
+                          .split(seperator);
 
             this.lineCache = [];
 
@@ -37,21 +42,25 @@ export class Config {
 
             for (const value of values) {
                 const keyValuePair = this.parseLine(value);
-                if (keyValuePair != null)
-                    parsed.push(keyValuePair[1])
+                if (keyValuePair != null) parsed.push(keyValuePair[1]);
             }
 
             return [key, parsed];
-        }
-
-        else if (line.includes('}')) {
+        } else if (line.includes('}')) {
             if (this.lineCache[0] !== '1') throw new Error('Invalid config syntax');
             // end of dict
             // process dict through lineCache
             const key = this.lineCache[1];
-            const values = seperator == null ? this.lineCache.slice(2, this.lineCache.length).filter(x => x !== '{') : this.lineCache.slice(2, this.lineCache.length).filter(x => x !== '{').join(seperator).trim().replace(/\s/g, '').split(seperator);
-
-            console.log(values);
+            const values =
+                seperator == null
+                    ? this.lineCache.slice(2, this.lineCache.length).filter((x) => x !== '{')
+                    : this.lineCache
+                          .slice(2, this.lineCache.length)
+                          .filter((x) => x !== '{')
+                          .join(seperator)
+                          .trim()
+                          .replace(/\s/g, '')
+                          .split(seperator);
 
             this.lineCache = [];
 
@@ -59,8 +68,7 @@ export class Config {
 
             for (const value of values) {
                 const keyValuePair = this.parseLine(value);
-                if (keyValuePair != null)
-                    parsed[keyValuePair[0]] = keyValuePair[1];
+                if (keyValuePair != null) parsed[keyValuePair[0]] = keyValuePair[1];
             }
 
             return [key, parsed];
@@ -72,10 +80,9 @@ export class Config {
         if (line.startsWith('#')) return;
 
         //! maybe this has to be checked at the end of this function to support inline lists / dicts
-        const dictOrList = this.processDictOrList(line)
+        const dictOrList = this.processDictOrList(line);
 
-        if (dictOrList != null)
-            return dictOrList;
+        if (dictOrList != null) return dictOrList;
 
         if (this.lineCache.length > 0) {
             // collecting data for parsing dict or list, return null to not add to config
@@ -87,80 +94,77 @@ export class Config {
         const lKey = lSplitted[0];
         const lValue = lSplitted.length > 1 ? lSplitted[1] : lSplitted[0];
 
-        console.log(lValue)
-
         if (lValue.startsWith('[') || lValue.startsWith('{')) {
             // begin of list (0) / dict (1)^
-            console.log('Begin of list / dict')
             this.lineCache = [];
-            this.lineCache.push(lValue.startsWith('{') ? '1' : '0')
-            this.lineCache.push(lKey)
-            this.lineCache.push((lValue.includes(']') || lValue.includes('}')) ? lValue : lValue.replace(/,/g, ''))
+            this.lineCache.push(lValue.startsWith('{') ? '1' : '0');
+            this.lineCache.push(lKey);
+            this.lineCache.push(lValue.includes(']') || lValue.includes('}') ? lValue : lValue.replace(/,/g, ''));
             return null;
         }
 
-        const dictOrListInline = this.processDictOrList(line, ',')
-        if (dictOrListInline != null)
-            return dictOrList;
+        const dictOrListInline = this.processDictOrList(line, ',');
+        if (dictOrListInline != null) return dictOrList;
 
-        return [lKey, this.parseValueUnknownType(lValue.lastIndexOf(',') == lValue.length - 1 ? lValue.substring(0, lValue.length - 1) : lValue)];
+        return [
+            lKey,
+            this.parseValueUnknownType(
+                lValue.lastIndexOf(',') == lValue.length - 1 ? lValue.substring(0, lValue.length - 1) : lValue,
+            ),
+        ];
     }
     protected parseValueUnknownType(value: string): any {
         for (const sType in ValueType) {
             const type = Number(sType);
-            if (isNaN(type)) return
+            if (isNaN(type)) return;
             try {
                 const parsedValue = this.parseValueOfType(type, value);
                 return parsedValue;
-            } catch (e) {
-                //console.log(e);
-            }
+            } catch (e) {}
         }
     }
     protected parseValueOfType(type: ValueType, value: string): any {
         if (type === ValueType.Boolean) {
             const val = Boolean(value);
-            if (typeof val !== 'boolean' || (value !== 'true' && value !== 'false' && value !== 'yes' && value !== 'no'))
+            if (
+                typeof val !== 'boolean' ||
+                (value !== 'true' && value !== 'false' && value !== 'yes' && value !== 'no')
+            )
                 throw new Error('Wrong type: boolean');
             return val;
         } else if (type === ValueType.Number) {
             const val = Number(value);
-            if (typeof val !== 'number' || isNaN(val))
-                throw new Error('Wrong type: number');
+            if (typeof val !== 'number' || isNaN(val)) throw new Error('Wrong type: number');
             return val;
         } else if (type === ValueType.String) {
             let val = String(value);
-            if (typeof val !== 'string')
-                throw new Error('Wrong type: string');
-            val = (val.indexOf('"') == 0 || val.indexOf("'") == 0) ? val.substring(1, val.length) : val;
-            val = (val.lastIndexOf('"') == val.length - 1 || val.lastIndexOf("'") == val.length - 1) ? val.substring(0, val.length - 1) : val;
+            if (typeof val !== 'string') throw new Error('Wrong type: string');
+            val = val.indexOf('"') == 0 || val.indexOf("'") == 0 ? val.substring(1, val.length) : val;
+            val =
+                val.lastIndexOf('"') == val.length - 1 || val.lastIndexOf("'") == val.length - 1
+                    ? val.substring(0, val.length - 1)
+                    : val;
             return val;
         } else if (type === ValueType.Dict) {
             // iterate over key: value pair
         } else if (type === ValueType.List) {
             // iterate over list
-
         }
     }
     protected parse(): void {
         this.config = {};
 
-        if (typeof (this.path) !== 'string') return;
+        if (typeof this.path !== 'string') return;
 
         const fileContent = fs.readFileSync(path.normalize(this.path), { encoding: 'utf8' });
 
         for (const line of fileContent.split('\n')) {
             const parsedLine = this.parseLine(line);
-            if (parsedLine != null)
-                this.config[parsedLine[0]] = parsedLine[1];
-            console.log('Parsed line', parsedLine, this.config)
+            if (parsedLine != null) this.config[parsedLine[0]] = parsedLine[1];
         }
-
     }
     get(key: string): any {
-        console.log('get key:', key);
-        console.log('config:', this.config);
-        return (key in this.config) ? this.config[key] : null;
+        return key in this.config ? this.config[key] : null;
     }
     has(key: string): boolean {
         return key in this.config;
@@ -174,4 +178,4 @@ export class Config {
     }
 }
 
-export default Config
+export default Config;
