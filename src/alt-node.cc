8d04c81958e9cd765b2714c71fdf366b3d49a45b
Timo972 <beckmann.timo@icloud.com>
2021-03-27 22:03:57 +0100

    fix building

--- src/alt-node.cc
+++ src/alt-node.cc
@@ -1,39 +1,13 @@
 #include <node.h>
-#include <napi.h>
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#define ISWIN
-#include <windows.h>
-typedef void (CALLBACK* NODE_MODULE_REGISTER_FUNC)(void *);
-#else
+#ifdef __unix__
+#define ISLINUX
 #include <dlfcn.h>
 #endif
 
-#ifdef ISWIN
-extern "C" __declspec(dllexport) void node_module_register(void *mod)
-{
-
-    auto base_ptr = GetModuleHandle("libnode.dll");
-    if (base_ptr == nullptr)
-    {
-        return;
-    }
-    auto register_func = (NODE_MODULE_REGISTER_FUNC)GetProcAddress(base_ptr, "node_module_register");
-
-    if (register_func == nullptr)
-    {
-        return;
-    }
-
-    register_func(mod);
-
-    //auto register_func = reinterpret_cast<decltype(&node_module_register)>(proc);
-
-    //register_func(mod);
-}
-#else
+#ifdef __unix__
 extern "C" NODE_EXTERN void node_module_register(void *mod)
 {
-    auto base_ptr = dlopen("libnode.so.72", RTLD_NOW | RTLD_GLOBAL);
+    auto base_ptr = dlopen("libnode.so.83", RTLD_NOW | RTLD_GLOBAL);
     if (base_ptr == nullptr)
     {
         return;
