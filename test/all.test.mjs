49b0effbb9a8da5a23ceb8a5dcb3b02e9237c436
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-07-17 17:32:56 +0200

    fix tests

--- test/all.test.mjs
+++ test/all.test.mjs
@@ -275,7 +275,7 @@ describe("New config", () => {
       assert.strictEqual("get" in config, true, "Invalid config instance");
     });
     it("Set value", () => {
-      const success = config.set("test", 'Hello World!');
+      const success = config.set("test", "Hello World!");
       assert.strictEqual(success, true, "Error while setting key/value");
     });
     it("Save config", () => {
@@ -284,10 +284,11 @@ describe("New config", () => {
     });
     it("Re-open config", () => {
       config = new Config(newConfigPath);
+      assert.strictEqual("get" in config, true, "Invalid config instance");
     });
     it("Check", () => {
       const val = config.get("test");
-      assert.strictEqual(val, true, "Changes not applied");
+      assert.strictEqual(val, "Hello World!", "Changes not applied");
     });
   });
 
@@ -296,10 +297,15 @@ describe("New config", () => {
     it("Create config", () => {
       if (fs.existsSync(newConfigPath)) fs.unlinkSync(newConfigPath);
       config = new Config(newConfigPath, { test: true });
+      assert.strictEqual("get" in config, true, "Invalid config instance");
+    });
+    it("Save config", () => {
+      const success = config.save();
+      assert.strictEqual(success, true, "Could not save changes to file");
     });
     it("Re-open config", () => {
-      const success = config = new Config(newConfigPath);
-      assert.strictEqual(success, true, "Could not save changes to file");
+      config = new Config(newConfigPath);
+      assert.strictEqual("get" in config, true, "Invalid config instance");
     });
     it("Check", () => {
       const val = config.get("test");
