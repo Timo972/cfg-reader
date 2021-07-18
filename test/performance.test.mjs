ac03871f5e2555f155985f911797716ea5012306
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-07-19 00:12:29 +0200

    add performance test

---
+++ test/performance.test.mjs
@@ -0,0 +1,51 @@
+import { Config, Type } from "../index.mjs";
+import { strict as assert } from "assert";
+import fs from "fs";
+
+class PerfTest {
+  constructor() {
+    this.begin = +new Date();
+  }
+  stop() {
+    return +new Date() - this.begin;
+  }
+}
+
+after(() => {
+  if (fs.existsSync("perf.cfg")) fs.unlinkSync("perf.cfg");
+});
+
+describe("Performance tests", () => {
+  let cfg;
+
+  it("Creating config", () => {
+    const test = new PerfTest();
+    cfg = new Config("perf.cfg", true);
+    assert.equal(test.stop() < 0.001, true, "Slow!");
+  });
+
+  it("Set string value", () => {
+    const test = new PerfTest();
+    cfg.set("test", "perf");
+    assert.equal(test.stop() < 0.001, true, "Slow!");
+  });
+
+  it("Get unknown value", () => {
+    const test = new PerfTest();
+    cfg.get("test");
+    assert.equal(test.stop() < 0.001, true, "Slow!");
+  });
+
+  it("Get string value", () => {
+    const test = new PerfTest();
+    cfg.getOfType("test", Type.String);
+    assert.equal(test.stop() < 0.001, true, "Slow!");
+  });
+
+  it("Save config", () => {
+    const test = new PerfTest();
+    cfg.save();
+    const time = test.stop();
+    assert.equal(time <= 1, true, `Slow! ${time}ms`);
+  });
+});
