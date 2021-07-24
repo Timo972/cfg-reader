be33610c50e1296f2dc9ee39b8f2ff8d8f2f13a7
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-07-24 19:36:29 +0200

    forgot to save alt-node helper

--- include/alt-node/helper.h
+++ include/alt-node/helper.h
@@ -11,8 +11,8 @@ namespace helper
 
     std::string Serialize(alt::config::Node node, bool useCommas = true, bool useApostrophe = true);
 
-    alt::config::Node SerializeValue(v8::Isolate* isolate, v8::Local<v8::Value> value);
+    alt::config::Node SerializeValue(v8::Isolate *isolate, v8::Local<v8::Value> value);
 
-    alt::config::Node V8ObjectToDict(v8::Isolate* isolate, v8::Local<v8::Object> object);
-    alt::config::Node V8ArrayToList(v8::Isolate* isolate, v8::Local<v8::Array> array);
+    alt::config::Node V8ObjectToDict(v8::Isolate *isolate, v8::Local<v8::Object> object);
+    alt::config::Node V8ArrayToList(v8::Isolate *isolate, v8::Local<v8::Array> array);
 }
