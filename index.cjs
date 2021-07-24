8670521ef48e292f20b7fa052ba3e79148d4a924
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-07-24 19:24:00 +0200

    copy binding getter from alt-node branch

--- index.cjs
+++ index.cjs
@@ -1,1 +1,5 @@
-module.exports = require('bindings')('config');
+const GetBinding = require('bindings');
+
+const altNode = process.moduleLoadList.includes('NativeModule alt');
+
+module.exports = altNode ? GetBinding("config") : GetBinding("config_alt");
--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
+
 import GetBinding from 'bindings';
 
-const bindings = GetBinding("config");
+const altNode = process.moduleLoadList.includes('NativeModule alt');
+
+const bindings = altNode ? GetBinding("config") : GetBinding("config_alt");
 
 export const Config = bindings.Config;
 export const Type = bindings.Type;
