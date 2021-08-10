1a6e164fada324b7fcefe973d8504393e3ae2162
Timo972 <beckmann.timo@icloud.com>
2021-08-10 14:48:14 +0200

    - install mocha-gulp - add tests to gulpfile - lower speed requirements because its completely dependent to hardware

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
-const { series } = require('gulp');
-const { getNode } = require('./scripts/get-node.cjs');
-const { build } = require('./scripts/build.cjs');
-const { clean } = require('./scripts/clean.cjs');
+const { series, src, parallel } = require("gulp");
+const mocha = require("gulp-mocha");
+const { getNode } = require("./scripts/get-node.cjs");
+const { build } = require("./scripts/build.cjs");
+const { clean } = require("./scripts/clean.cjs");
 
 exports.clean = clean;
 exports.build = series(build(false), clean);
 exports.buildAlt = series(getNode, build(true), clean);
+
+const testUnit = () => src("test/unit.test.mjs", { read: false }).pipe(
+  mocha({
+    reporter: "nyan",
+  })
+);
+
+const testPerformance = () => src("test/performance.test.mjs", { read: false }).pipe(
+    mocha({
+      reporter: "nyan",
+    })
+);
+
+exports.test = series(exports.build, testUnit);
+exports.testPerformance = series(exports.build, testPerformance);
+  
+exports.testAll = series(
+  exports.build,
+  parallel(testUnit, testPerformance)
+);
--- test/performance.test.mjs
+++ test/performance.test.mjs
@@ -21,31 +21,31 @@ describe("Performance tests", () => {
   it("Creating config", () => {
     const test = new PerfTest();
     cfg = new Config("perf.cfg", true);
-    assert.equal(test.stop() < 0.001, true, "Slow!");
+    assert.equal(test.stop() < 0.01, true, "Slow!");
   });
 
   it("Set string value", () => {
     const test = new PerfTest();
     cfg.set("test", "perf");
-    assert.equal(test.stop() < 0.001, true, "Slow!");
+    assert.equal(test.stop() < 0.01, true, "Slow!");
   });
 
   it("Get unknown value", () => {
     const test = new PerfTest();
     cfg.get("test");
-    assert.equal(test.stop() < 0.001, true, "Slow!");
+    assert.equal(test.stop() < 0.1, true, "Slow!");
   });
 
   it("Get string value", () => {
     const test = new PerfTest();
     cfg.getOfType("test", Type.String);
-    assert.equal(test.stop() < 0.001, true, "Slow!");
+    assert.equal(test.stop() < 0.1, true, "Slow!");
   });
 
   it("Save config", () => {
     const test = new PerfTest();
     cfg.save();
     const time = test.stop();
-    assert.equal(time <= 1, true, `Slow! ${time}ms`);
+    assert.equal(time <= 3, true, `Slow! ${time}ms`);
   });
 });
